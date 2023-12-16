2ddfb1037bb28b3e2e835d4c3a914b934b5d6639
Giorgio <tran.giorgio@gmail.com>
2023-12-16 01:08:35 -1000

    Add projects file

---
+++ portfolio/src/app/data/projects.js
@@ -0,0 +1,71 @@
+export const projects = [
+  {
+    name: "Makai",
+    image: "",
+    description:
+      "3rd place finish at the 2023 Hawaii Annual Code Challenge. Makai is a web application that facilitates marine debris tracking and removal in Hawaii. I designed the architecture of the application, and worked on both frontend and backend. On the frontend side, I worked on creating map based data visualizations, and creating a user friendly interface for users to interact with. On the backend side, I implemented authorization and authentication through NextAuth, and worked on various API endpoints.",
+    technologies: [
+      "React",
+      "Javascript",
+      "Git",
+      "Next.js",
+      "MongoDB",
+      "Node.js",
+      "Mapbox",
+      "Tailwind CSS",
+      "Dialogflow CX",
+    ],
+    links: [
+      {
+        name: "Live Demo",
+        url: "https://makai-marine.vercel.app/",
+      },
+      {
+        name: "Github",
+        url: "https://github.com/HACC-2023/makai",
+      },
+    ],
+  },
+  {
+    name: "DOELT",
+    image: "",
+    description:
+      "2nd place finish at the 2022 Hawaii Annual Code Challenge. The Department of Education Legislative Tracker (DOELT) is a web application that helps the DOE track and manage legislation. I gathered user needs and architected the user experience. Additionally, I implemented a scraper that scrapes several pages in the Hawaii State Legislature website for bills and their related information, and further assisted with frontend and backend development.",
+    technologies: [
+      "React",
+      "Javascript",
+      "Git",
+      "Meteor",
+      "MongoDB",
+      "Cheerio",
+      "Node.js",
+      "Express",
+    ],
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/HACC-Cassiopeia/Legislative-Scraper-Production",
+      },
+    ],
+  },
+  {
+    name: "Mejiro",
+    image: "",
+    description:
+      "October 2023 PACE Kalo Grant Awardee. Mejiro is a web application that parses receipts through optical character recognition (OCR) and generative AI, which ultimately aims to helps users track their spending patterns. This project is still ongoing and I have been mainly working on the frontend.",
+    technologies: [
+      "React",
+      "Javascript",
+      "Git",
+      "Tesseract",
+      "ChatGPT API",
+      "Tailwind CSS",
+    ],
+    links: [
+      {
+        name: "In Progress...",
+        url: "#",
+      },
+    ],
+  },
+];
